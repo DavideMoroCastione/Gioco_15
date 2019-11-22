96b306ac8bb92c22da9a04905b8c5ed32531c6c2
Davide <davide.moro@barsanti.edu.it>
2019-11-22 08:57:33 +0100

    22 - 11- 2019

--- GiocoDel15_Davide Moro/ViewController.swift
+++ GiocoDel15_Davide Moro/ViewController.swift
@@ -34,8 +34,8 @@ class ViewController: UIViewController {
     var mosse = 0
     var numero = 0
     var a = 0
-    var x = 3
-    var y = 3
+    var x = 0
+    var y = 0
     var mat: [[Int]] = [[1, 2, 3, 4],
                         [5, 6, 7, 8],
                         [9, 10, 11, 12],
@@ -46,6 +46,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         matrice = [[Btn1, Btn2, Btn3, Btn4],
                    [Btn5, Btn6, Btn7, Btn8],
                    [Btn9, Btn10, Btn11, Btn12],
@@ -55,11 +56,12 @@ class ViewController: UIViewController {
     }
     
     
-    
-    
     @IBAction func btn_start(_ sender: Any) {
         
     }
+    
+    
+    
     func contaMosse() {
         mosse = mosse + 1
         lblMoves.text = String(mosse)
@@ -70,21 +72,22 @@ class ViewController: UIViewController {
         for _ in 0 ... 5 {
             numero = Int(arc4random() % 2 + 1)
             
-            if(numero == 1 && x >= 0) {
-                x = x - 1
+            if(numero == 1 && x <= 3) {
+                x = x + 1
                 a = mat[x][y]
-                mat[x][y] = mat[x+1][y]
-                mat[x+1][y] = a
+                mat[x][y] = mat[x-1][y]
+                mat[x-1][y] = a
             }
-            else if(numero == 2 && y >= 0 ){
-                y = y - 1
+            else if(numero == 2 && y <= 3 ){
+                y = y + 1
                 a = mat[x][y]
-                mat[x][y] = mat[x][y+1]
-                mat[x][y+1] = a
+                mat[x][y] = mat[x][y-1]
+                mat[x][y-1] = a
             }
             else {}
         }
     }
+    
     
     func BtnLocked() {
         Btn1.isUserInteractionEnabled = false;
@@ -104,6 +107,5 @@ class ViewController: UIViewController {
         Btn15.isUserInteractionEnabled = false;
         BtnNull.isUserInteractionEnabled = false;
     }
-    
 }
 
