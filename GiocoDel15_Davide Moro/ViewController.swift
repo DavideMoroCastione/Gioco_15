a3a7b2a57c0fd094288b3726e72efb41eb8b9d94
Davide <davide.moro@barsanti.edu.it>
2019-11-27 12:43:03 +0100

    Lavoro - 27/11/2019

--- GiocoDel15_Davide Moro/ViewController.swift
+++ GiocoDel15_Davide Moro/ViewController.swift
@@ -30,17 +30,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var Btn14: UIButton!
     @IBOutlet weak var Btn15: UIButton!
     @IBOutlet weak var BtnNull: UIButton!
+    @IBOutlet weak var BtnReset: UIButton!
     
     var mosse = 0
     var numero = 0
-    var a = 0
-    var x = 0
-    var y = 0
-    var mat: [[Int]] = [[1, 2, 3, 4],
-                        [5, 6, 7, 8],
-                        [9, 10, 11, 12],
-                        [13, 14, 15, 16]]
-    
+    var a = ""
+    var x = 3
+    var y = 3
     var matrice: [[UIButton]] = [[]]
     
     override func viewDidLoad() {
@@ -54,10 +50,15 @@ class ViewController: UIViewController {
         BtnLocked()
         
     }
+    @IBAction func btn_reset(_ sender: Any) {
+        resetta()
+        btnStart.isUserInteractionEnabled = true
+    }
     
     
     @IBAction func btn_start(_ sender: Any) {
-        
+        mischia()
+        btnStart.isUserInteractionEnabled = false
     }
     
     
@@ -68,23 +69,30 @@ class ViewController: UIViewController {
     }
     
     func mischia() {
-        //matrice con interi
-        for _ in 0 ... 5 {
+        //matrice con bottoni
+        for _ in 0 ... 10 {
+            
             numero = Int(arc4random() % 2 + 1)
             
-            if(numero == 1 && x <= 3) {
-                x = x + 1
-                a = mat[x][y]
-                mat[x][y] = mat[x-1][y]
-                mat[x-1][y] = a
+            if(numero == 1 && x >= 1) {
+                
+                a = matrice[x][y].currentTitle!
+                matrice[x][y].setTitle(String(matrice[x - 1][y].currentTitle!), for: .normal)
+                matrice[x - 1][y].setTitle(String(a), for: .normal)
+                x = x - 1
             }
-            else if(numero == 2 && y <= 3 ){
-                y = y + 1
-                a = mat[x][y]
-                mat[x][y] = mat[x][y-1]
-                mat[x][y-1] = a
+            else if(numero == 2 && y >= 1 ){
+                
+                a = matrice[x][y].currentTitle!
+                matrice[x][y].setTitle(String(matrice[x][y - 1].currentTitle!), for: .normal)
+                matrice[x][y - 1].setTitle(String(a), for: .normal)
+                y = y - 1
             }
-            else {}
+            else {
+                
+                if(x == 1){ x = x + 1 }
+                else if(y == 1){ y = y + 1 }
+            }
         }
     }
     
@@ -106,6 +114,25 @@ class ViewController: UIViewController {
         Btn14.isUserInteractionEnabled = false;
         Btn15.isUserInteractionEnabled = false;
         BtnNull.isUserInteractionEnabled = false;
+    }
+    
+    func resetta() {
+        Btn1.setTitle(String(1), for: .normal)
+        Btn2.setTitle(String(2), for: .normal)
+        Btn3.setTitle(String(3), for: .normal)
+        Btn4.setTitle(String(4), for: .normal)
+        Btn5.setTitle(String(5), for: .normal)
+        Btn6.setTitle(String(6), for: .normal)
+        Btn7.setTitle(String(7), for: .normal)
+        Btn8.setTitle(String(8), for: .normal)
+        Btn9.setTitle(String(9), for: .normal)
+        Btn10.setTitle(String(10), for: .normal)
+        Btn11.setTitle(String(11), for: .normal)
+        Btn12.setTitle(String(12), for: .normal)
+        Btn13.setTitle(String(13), for: .normal)
+        Btn14.setTitle(String(14), for: .normal)
+        Btn15.setTitle(String(15), for: .normal)
+        BtnNull.setTitle(String("--"), for: .normal)
     }
 }
 
